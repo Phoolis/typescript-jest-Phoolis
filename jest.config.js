9b771ecc4c5de7b1228d68c6c0f9991e285f0fe3
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2023-02-03 10:11:26 +0200

    Poistettu tarpeeton isolatedModules jest-konfiguraatiosta

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,4 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/bin/', '/build/', '/.git/'],
-
-  // see https://kulshekhar.github.io/ts-jest/docs/getting-started/options/isolatedModules/
-  transform: {
-    '^.+\\.m?[tj]sx?$': [
-      'ts-jest',
-      {
-        isolatedModules: true,
-      },
-    ],
-  },
 };
